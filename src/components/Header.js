f129a85ef597b5508344059d963d80554af91be0
KelvinHalx <kelvinwainaina03@gmail.com>
2022-08-18 13:48:09 +0300

    added navbar and all components

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+import sparepap from './sparepap.png';
+
+function Header() {
+  return (
+    <div className='container shadow-lg'>
+    {/* logo */}
+
+    {/* center */}
+
+    {/* right */}
+    <nav className="navbar navbar-expand-lg ">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+    <img src={sparepap} width="30" height="30" alt=""/>
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <form className="d-flex mx-auto">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active nav-text" aria-current="page" href="#">Find Mechanic</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active nav-text" href="#">Book Service</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link active dropdown-toggle nav-text" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Products
+          </a>
+          {/* <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"></hr></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul> */}
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link active dropdown-toggle nav-text" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Accounts
+          </a>
+          {/* <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"></hr></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul> */}
+        </li>
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+
+
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className=''>sidebar</div>
+            <div className=''>Products</div>
+
+        </div>
+        {/* sidebar */}
+        {/* Products */}
+      
+    </div>
+  )
+}
+
+export default Main
